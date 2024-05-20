f57a27c16bffc83b31f714016fd65398b88361b8
yoanvlt <113009422+yoanvlt@users.noreply.github.com>
2024-05-20 19:27:56 +0200

    mise en place de la connexion a la db

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const { testConnection } = require('./src/config/db');
 const app = express();
 const port = 3000;
+
 
 // Middleware pour servir les fichiers statiques
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Importation des routeurs
-const homeRouter = require('./routes/homeRoutes');
-const wishlistRouter = require('./routes/wishlistRoutes');
-const newsRouter = require('./routes/newsRoutes');
-const accountRouter = require('./routes/accountRoutes');
-const collectionRouter = require('./routes/collectionRoutes');
+const homeRouter = require('./src/routes/homeRoutes');
+const wishlistRouter = require('./src/routes/wishlistRoutes');
+const newsRouter = require('./src/routes/newsRoutes');
+const accountRouter = require('./src/routes/accountRoutes');
+const collectionRouter = require('./src/routes/collectionRoutes');
 
 // Utilisation des routeurs
 app.use('/', homeRouter);
@@ -19,6 +22,9 @@ app.use('/wishlist', wishlistRouter);
 app.use('/news', newsRouter);
 app.use('/account', accountRouter);
 app.use('/collection', collectionRouter);
+
+// Test de connexion DB
+testConnection();
 
 // Démarrage du serveur
 app.listen(port, () => {
--- src/routes/wishlistRoutes.js
+++ src/routes/wishlistRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const path = require('path');
 
 // Route pour la page d'accueil
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'home.html'));
+router.get('/wishlist', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'wishlist.html'));
 });
 
 module.exports = router;
