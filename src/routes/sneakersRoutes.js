cd7a2a6e98fd48e31746b7b3449ce91cbe920c88
yoanvlt <113009422+yoanvlt@users.noreply.github.com>
2024-05-28 22:26:01 +0200

    actualisation de la page sneaker en fonction de l id

--- src/routes/sneakersRoutes.js
+++ src/routes/sneakersRoutes.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const data = require('../data/data.json');
 
-// Route pour la page d'accueil
-router.get('/', (req, res) => {
+// API route for getting sneaker details by ID
+router.get('/api/:id', (req, res) => {
+    const sneaker = data.sneakers.find(s => s.id.toString() === req.params.id);
+    if (sneaker) {
+        res.json(sneaker);
+    } else {
+        res.status(404).send('Sneaker not found');
+    }
+});
+
+// Route to serve the sneakers.html page
+router.get('/:id', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'sneakers.html'));
 });
 
